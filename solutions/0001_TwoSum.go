6806b7fb1e1d114f1ae6dac88d25212811265d90
Hamid Zamani <hamid.zamani@endava.com>
2024-10-02 17:46:16 +0200

    a small refactoring

--- solutions/0001_TwoSum.go
+++ solutions/0001_TwoSum.go
@@ -1,12 +1,12 @@
 package solutions
 
 func TwoSum(nums []int, target int) []int {
-	lookingNums := make(map[int]int)
+	seenNumsMap := make(map[int]int)
 	for i := 0; i < len(nums); i++ {
-		if _, ok := lookingNums[target-nums[i]]; ok {
-			return []int{lookingNums[target-nums[i]], i}
+		if _, ok := seenNumsMap[target-nums[i]]; ok {
+			return []int{seenNumsMap[target-nums[i]], i}
 		}
-		lookingNums[nums[i]] = i
+		seenNumsMap[nums[i]] = i
 	}
 
 	return []int{}
